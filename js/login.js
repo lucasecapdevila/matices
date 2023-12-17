50cab7fee02bc3f60c4ab8b123f13f42e1c1d048
DylanSDev <dylansdiaz.dev@gmail.com>
2023-12-17 12:46:53 -0300

    Se agrego la logica de login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+/* Login */
 /* Validación Email */
 let email = document.getElementById("inputEmail");
 let errorEmail = document.getElementById("errorEmail");
@@ -39,13 +40,13 @@ class Usuario {
   }
 }
 
-let admin1 = new Usuario("superadmin1@admin.com", "12345678");
+let admin1 = new Usuario("superadmin1@admin.com", "admin@404_2qwe");
 let admin2 = new Usuario("superadmin2@admin.com", "12345678");
 
-// Obtener usuarios existentes del local storage
+// Obtenemos usuarios existentes del local storage
 let usuariosGuardados = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-// Verificar si los usuarios existentes son diferentes de los nuevos usuarios
+// Verificamos si los usuarios existentes son diferentes de los nuevos usuarios
 const usuariosDiferentes =
   !usuariosGuardados.some(
     (usuario) =>
@@ -72,6 +73,7 @@ usuariosGuardados.forEach((usuario) => {
 formulario = document.getElementById("form");
 
 formulario.addEventListener("submit", (e) => {
+  let token = false;
   e.preventDefault();
   if (validationMail && validationPassword) {
     // Traemos los datos del local storage
@@ -87,12 +89,104 @@ formulario.addEventListener("submit", (e) => {
           usuario.email === email.value &&
           usuario.password === password.value
         ) {
-          console.log("Ingresando al sistema.");
-          window.location.href = "../pages/administracion.html";
+          Swal.fire({
+            title: '<h2 class="tx-titulo text-center">¡Bienvenido!</h2>',
+            text: "¡Iniciaste sesión!",
+            icon: "success",
+            confirmButtonText: "Continuar",
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+
+            customClass: {
+              popup: "rounded-5",
+              confirmButton: "btn-modal",
+            },
+          }).then((result) => {
+            if (result.isConfirmed) {
+              console.log("Ingresando al sistema.");
+              window.location.href = "../pages/administracion.html";
+            }
+          });
+          token = true;
+          localStorage.setItem("logged", "true");
         }
       });
+
+      if (token === false) {
+        Swal.fire({
+          title:
+            '<h2 class="tx-titulo text-center">¡No pudimos iniciar sesión!</h2>',
+          text: "Los datos que ingresaste son incorrectos...",
+          icon: "error",
+          confirmButtonText: "Reintentar",
+          confirmButtonColor: "#f12b2b",
+
+          customClass: {
+            popup: "rounded-5",
+          },
+        });
+      }
     } else {
       console.log("No hay usuarios almacenados en el localStorage.");
     }
   }
 });
+
+/* Navbar logica */
+const navbar = document.getElementById("lista-navbar");
+
+function navbar_admin() {
+  let logged = localStorage.getItem("logged");
+
+  if (logged) {
+    navbar.innerHTML += `<li class="nav-item ms-auto mb-2" id="administracion-navbar">
+    <a
+      class="hvr-underline-from-left nav-link tx-RosaMatices fs-5"
+      href="./administracion.html"
+      >Administración</a
+    >
+  </li>`;
+  }
+}
+
+navbar_admin();
+
+/* Loggin desactivado */
+const contenedorLogin = document.getElementById("container-login");
+const admin_navbar = document.getElementById("administracion-navbar");
+
+function desactivarLogin() {
+  let logged = localStorage.getItem("logged");
+  console.log(localStorage);
+  if (logged) {
+    Swal.fire({
+      icon: "success",
+      title: '<h2 class="tx-titulo text-center">¡Ya iniciaste sesión!</h2>',
+      text: "¿Quierés cerrar la sesión actual?",
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: "Cancelar",
+      denyButtonText: "Cerrar Sesión",
+      customClass: {
+        popup: "rounded-5",
+        confirmButton: "btn-modal rounded-4",
+        denyButton: "rounded-4",
+      },
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        window.location.href = "../pages/administracion.html";
+      } else if (result.isDenied) {
+        localStorage.setItem("logged", "false");
+        admin_navbar.remove();
+      }
+    });
+  } else {
+  }
+}
+
+desactivarLogin();
